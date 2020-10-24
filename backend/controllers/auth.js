d60ec55b046c9faff18eed3cdd2df6305fb4861b
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-24 17:39:29 +0530

    user profile read and update

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -84,3 +84,28 @@ exports.signout = (req, res) => {
         message: "logout success"
     })
 }
+exports.requireSignin = expressJWT({
+    secret: JWT_SECRET,
+    userProperty: "auth",
+    algorithms: ['HS256']
+})
+
+exports.isAuth = (req, res, next) => {
+    let user = req.profile && req.auth && req.profile._id == req.auth._id;
+    if (!user) {
+        return res.status(403).json({
+            error: "Access Denied"
+        })
+    }
+    next();
+}
+
+exports.isAdmin = (req, res, next) => {
+    if (req.profile.role === 0) {
+        return res.status(403).json({
+            error: 'admin resource!!  access denied '
+        })
+    }
+    next();
+}
+
--- backend/routers/user.js
+++ backend/routers/user.js
@@ -1,4 +1,25 @@
 const express = require('express')
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
+const { read, update, userById } = require('../controllers/user')
+
+
 const router = express.Router()
 
 
+
+
+
+
+
+router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
+    res.json({
+        user: req.profile
+    })
+})
+
+router.get('/user/:userId', requireSignin, isAuth, read)
+router.put('/user/:userId', requireSignin, isAuth, update)
+
+router.param('userId', userById)
+
+module.exports = router;
