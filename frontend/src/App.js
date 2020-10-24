ee02eeb198d6d182fa85d84180c5c861b498a884
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-24 12:28:33 +0530

    deleted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,65 +1,23 @@
-import React from 'react';
-import  { useState } from "react";
+import logo from './logo.svg';
 import './App.css';
-import Mainpage from "./Components/Mainpage"
-import {BrowserRouter as Router,Route,Switch} from "react-router-dom"
-import SimpleList1 from './Components/SimpleList1';
-import SignIn from './Components/SignIn';
-import SignUp from "./Components/SignUp"
-import Header from "./Components/Header"
-import 'semantic-ui-css/semantic.min.css'
-import cn from "classnames"
-import SideBar1 from "./Components/SideBar1"
-import Cards1 from "./Components/Cards1"
-import CartScreen from "./Components/CartScreen"
-import categoryScreen from './Components/CategoryScreen';
-import OnGoingScreen from "./Components/OnGoingScreen"
-import FindWork from "./Components/FindWork"
-import CompletedWorks from "./Components/CompletedWorks"
-import PostDetailsScreen from "./Components/PostDetailsScreen"
-
-
-
 
 function App() {
-  const [toggle,setToggle] = useState(true);
-
-  const classes = cn('pusher','bottom',{'dimmed': toggle})
-  
-      function toggleMenu(){
-          console.log("toggled")
-        setToggle(!toggle)
-      
-      }
-
   return (
-    
     <div className="App">
-      <Router>
-                <Route exact={true} path="/"  component={Mainpage} />    
-                <Route exact={true} path="/signin" component={SignIn}/>
-                <Route exact={true} path="/signup" component={SignUp}/>
-      <div >          
-           <Header onToggleMenu={toggleMenu}/> 
-           <div className="ui attached pushable" style={{height:650}}> 
-                <SideBar1 toggleMenu={toggle}/>
-          
-           <main>
-           <Switch>
-              
-                <Route  path="/cart/:id?" component={CartScreen}/>
-                <Route  path="/category" component={categoryScreen}/>
-                <Route  path="/ongoing/" component ={OnGoingScreen}/>
-                <Route  path="/findwork" component={FindWork}/>
-                <Route  path="/completed" component={CompletedWorks}/>
-                <Route  path="/category/:id" component={PostDetailsScreen}/>
-          </Switch>
-           </main>
-           </div>
-
-       </div>
-      </Router>
-  
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- frontend/src/constants/cartConstants.js
+++
@@ -1,4 +0,0 @@
-export const CART_ADD_POST  ="CART_ADD_ITEM";
-export const CART_REMOVE_POST  ="CART_REMOVE_ITEM";
-
-
