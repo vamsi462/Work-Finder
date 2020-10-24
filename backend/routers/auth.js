b57cad1caf0165802ab4d96e35ee1a2c299df722
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-24 16:41:48 +0530

    changing the files name

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -1,2 +1,9 @@
 const express = require('express')
+const { signup } = require('../controllers/auth')
 const router = express.Router()
+const {userSignUpValidator} = require('../validator');
+
+
+router.post('/signup', signup, userSignUpValidator)
+
+module.exports= router;
--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const morgan = require('morgan');
+const authRoutes = require('./backend/routers/auth');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+const connectDB = require("./backend/database/db"); 
 
 const app = express();
 
 
-const connectDB = require('./backend/database/db'); 
+
 //database connection..
 connectDB()
 
 //middlewares
-
 app.use(morgan('dev'))
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+
+
+//routers middleware
+
+app.use('/api', authRoutes)
 
 
 const port = process.env.PORT || 9000;
