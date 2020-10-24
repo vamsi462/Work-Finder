a8bd0cbf9ca92f0a8bdf5e62e215afe447d3b7e5
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-24 17:38:42 +0530

    user profile updation

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const morgan = require('morgan');
-const authRoutes = require('./backend/routers/auth');
+
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator');
 const connectDB = require("./backend/database/db"); 
-
 const app = express();
-
+const authRoutes = require('./backend/routers/auth');
+const userRoutes = require('./backend/routers/user');
 
 
 //database connection..
@@ -23,6 +23,8 @@ app.use(expressValidator());
 //routers middleware
 
 app.use('/api', authRoutes)
+app.use('/api', userRoutes)
+
 
 
 const port = process.env.PORT || 9000;
