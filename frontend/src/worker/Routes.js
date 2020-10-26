f2c3e16d44974cc2095c2d8a17bc95877c6a68f9
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-26 22:41:25 +0530

    worker can update his profile

--- frontend/src/worker/Routes.js
+++ frontend/src/worker/Routes.js
@@ -6,6 +6,8 @@ import Home from "../core/Home";
 import WorksStore from "../core/WorksStore";
 import PrivateRoute from "../auth/PrivateRoute";
 import Dashboard from "./WorkerDashBoard";
+import Profile from "./Profile";
+import Cart from "../core/Cart";
 
 const Routes = () => {
         return ( 
@@ -14,8 +16,11 @@ const Routes = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/signup" component={SignUp} />
                 <Route  path ="/signin"  exact component={SignIn}/>
+                         <Route  path="/cart" exact component={Cart} />
                  <Route exact path="/worksByCategories" component={WorksStore} />
                   <PrivateRoute path="/user/dashboard" exact component={Dashboard}/>
+                  <PrivateRoute path="/profile/:userId" exact component={Profile}/>
+
             </Switch>
     </BrowserRouter>
   );
