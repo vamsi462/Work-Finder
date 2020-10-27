b7f9ec18cb2ebc919b9fc500edaf1a8dba911121
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-27 16:45:15 +0530

    accepting the work that the worker intrested in

---
+++ frontend/src/WorkerProvider/ViewAcceptedWorks.js
@@ -0,0 +1,157 @@
+import React, { useState, useEffect } from 'react'
+import Layout from '../core/Layout';
+import { isAuthenticated } from '../auth';
+import moment from 'moment'
+import { updateWorkStatus, list, getStatusValues, } from './WorkProviderAPI';
+
+
+const ViewAcceptedWorks = () => {
+
+    const [acceptedworks, setAcceptedWorks] = useState([]);
+     const [statusValues, setStatusValues] = useState([]);
+
+    const {user,token}= isAuthenticated();
+
+    const loadWorks = () => {
+        list(user._id, token).then(data => {
+            if(data){
+                setAcceptedWorks(data);
+            }
+        });
+    };
+    const loadStatusValues = () => {
+        getStatusValues(user._id, token).then(data => {
+            if(data) {
+                setStatusValues(data);
+            }
+        });
+    };
+
+     useEffect(() => {
+         loadWorks();
+         loadStatusValues();
+     }, []);
+
+
+     const showWorksLength = () => {
+        if (acceptedworks.length > 0) {
+            return (
+                <h3 className="text-danger display-4">
+                    Total works accepted by the worker: {acceptedworks.length}
+                </h3>
+            );
+        } else {
+            return <h3 className="text-danger">No Accepted orders</h3>;
+        }
+    };
+    const showInput = (key, value) => (
+        <div className="input-group mb-2 mr-sm-2">
+            <div className="input-group-prepend">
+                <div className="input-group-text">{key}</div>
+            </div>
+            <input
+                type="text"
+                value={value}
+                className="form-control"
+                readOnly
+            />
+        </div>
+    );
+    const handleStatusChange = (e, acceptWorkId) => {
+        updateWorkStatus(user._id, token, acceptWorkId, e.target.value).then(
+            data => {
+                if (data.error) {
+                    console.log("Status update failed");
+                } else {
+                    loadWorks();
+                }
+            }
+        );
+        console.log('updated the status')
+    };
+     const showStatus = o => (
+        <div className="form-group">
+            <h3 className="mark mb-4">Status: {o.status}</h3>
+            <select
+                className="form-control"
+                onChange={e => handleStatusChange(e, o._id)}
+            >
+                <option>Update Status</option>
+                {statusValues.map((status, index) => (
+                    <option key={index} value={status}>
+                        {status}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+
+    return (
+        <Layout
+            title="Accepted Works"
+            description={`G'day ${
+                user.name
+            }, you can manage all the orders here`}
+            className="container-fluid"
+        >
+        <div className="row">
+                <div className="col-md-8 offset-md-2">
+                    {showWorksLength()}
+
+                    {acceptedworks.map((o, oIndex) => {
+                        return (
+                            <div
+                                className="mt-5"
+                                key={oIndex}
+                                style={{ borderBottom: "5px solid indigo" }}
+                            >
+                                <h2 className="mb-5">
+                                    <span className="bg-primary">
+                                        Order ID: {o._id}
+                                    </span>
+                                </h2>
+
+                                <ul className="list-group mb-2">
+                                    <li className="list-group-item">
+                                        {showStatus(o)}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Amount: ${o.wage}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Accepted by: {o.user.name}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Accepted on:{" "}
+                                        {moment(o.createdAt).fromNow()}
+                                    </li>
+                                </ul>
+
+                                <h3 className="mt-4 mb-4 font-italic">
+                                    Total works in the order:{" "}
+                                    {o.works.length}
+                                </h3>
+
+                                {o.works.map((p, pIndex) => (
+                                    <div
+                                        className="mb-4"
+                                        key={pIndex}
+                                        style={{
+                                            padding: "20px",
+                                            border: "1px solid indigo"
+                                        }}
+                                    >
+                                        {showInput(" worktype", p.worktype)}
+                                        {showInput(" wage", p.wage)}
+                                    </div>
+                                ))}
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+    </Layout>
+    )
+}
+
+export default ViewAcceptedWorks
---
+++ frontend/src/core/checkout.js
@@ -0,0 +1,97 @@
+import React,{ useState } from "react"
+import { Link } from "react-router-dom";
+import { isAuthenticated } from "../auth";
+import { emptyCart } from "./cartHelpers";
+import { acceptWork } from "./apiCore";
+
+
+const Checkout = ({works}) => {
+     const [data, setData] = useState({
+         loading: false,
+         success: false,
+         error: '',
+         instance: {},
+     });
+    
+
+    const userId = isAuthenticated() && isAuthenticated().user._id;
+    const token = isAuthenticated() && isAuthenticated().token;
+
+         const showCheckout = () => {
+        return isAuthenticated() ?(
+         <div> {
+            <Link to="/user/dashboard">
+                <button className="btn btn-primary">View Your Status</button>
+            </Link>
+         } </div>
+        ) : (
+            <Link to="/signin">
+                <button className="btn btn-primary">Sign in to accept</button>
+            </Link>
+        );
+    };
+
+const accept = () => {
+                    setData({
+                        loading: true
+                    })
+                    const acceptWorkData  = {
+                        works: works,
+                    };
+                    
+                    acceptWork(userId, token, acceptWorkData)
+                        .then(response => {
+                            emptyCart(() => {
+                                //setRun(!run); // run useEffect in parent Cart
+                                console.log(' accepted and  empty cart');
+                                setData({
+                                    loading: false,
+                                    success: true
+                                });
+                            });
+                        })
+                        .catch(error => {
+                            console.log(error);
+                            setData({
+                                loading: false
+                            });
+                        });
+      
+};
+
+    
+    const showAccept = () => (
+            
+                    <button onClick={accept}  className="btn btn-success btn-block">
+                        Accept
+                    </button>
+                
+            )
+
+      const showError = error => (
+        <div className="alert alert-danger" style={{ display: error ? '' : 'none' }}>
+            {error}
+        </div>
+    );
+    const showSuccess = success => (
+        <div className="alert alert-info" style={{ display: success ? '' : 'none' }}>
+            Thanks! You have accepted this work!
+        </div>
+    );
+
+    const showLoading =(loading)=>loading&& <h2>loading....</h2>
+    return (
+        <div>
+          {showLoading(data.loading)}
+            { showSuccess(data.success)}
+            {showError(data.error)}
+         
+          {showAccept()}
+         
+          {showCheckout()}
+
+        </div>
+    )
+}
+
+export default Checkout
